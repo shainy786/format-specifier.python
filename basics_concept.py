f199961e0530ad1e36a6d47ebb041c7906b9fd8f
Shainaz <102401756+shainy786@users.noreply.github.com>
2022-09-19 03:43:22 -0700

    Create basics_concept.py

---
+++ basics_concept.py
@@ -0,0 +1,12 @@
+#"for integers we represented by %d or %i"
+a=5
+print("%d" %a)
+print("%i" %a)
+#"for float we reprsented by %f"
+b=7
+print("%f"%b)              #note:after (.) automatically 6 zeroes added
+c=7.9
+print("%f"%c)              #note:after (.)one value is there so only 5 zeroes added
+#"for str we represented with %s"
+e="shainaz"
+print("%s"%e)
